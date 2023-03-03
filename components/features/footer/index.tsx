379f4f82495a5b124ad6f482505dfd82b9fab0b4
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-03-02 20:09:48 -0500

    add basic footer links and style

--- components/features/footer/index.tsx
+++ components/features/footer/index.tsx
@@ -1,6 +1,30 @@
 function Footer() {
+  const fontClassNames = "text-white font-bold";
   return (
-    <div>Footer</div>
-  )
+    <footer className="flex flex-row bg-slate-600 justify-center space-x-12 p-4">
+      <div className="flex flex-col ">
+        <a className={fontClassNames} href="#">
+          Help
+        </a>
+        <a className={fontClassNames} href="#">
+          Personal Data Policy
+        </a>
+        <a className={fontClassNames} href="#">
+          Log In
+        </a>
+      </div>
+      <div className="flex flex-col ">
+        <a className={fontClassNames} href="#">
+          Privacy Policy
+        </a>
+        <a className={fontClassNames} href="#">
+          Cookie Policy
+        </a>
+        <a className={fontClassNames} href="#">
+          Terms of Service
+        </a>
+      </div>
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
